6a59ea2daf80d7d635f79ea0c1974525a7f92a4e
John Daise <jdaise@gmail.com>
2018-03-05 23:47:48 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,6 +71,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.index("BBQ") {|season, holidays| holidays[:supplies].key(value) == "BBQ"}
+  holiday_hash.index("BBQ") {|season, holidays| holidays[:supplies] == "BBQ"}
 
 end
