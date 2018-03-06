035329a9995ea99bf92e5d5a000d05f6b3b3632d
John Daise <jdaise@gmail.com>
2018-03-06 00:31:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -71,7 +71,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.find{|season, holidays| holidays[:supplies] == "BBQ"}
+  holiday_hash.index{|season, holidays| holidays[:supplies] == "BBQ"}
 
   #holiday_hash.invert #["BBQ"]
 end
